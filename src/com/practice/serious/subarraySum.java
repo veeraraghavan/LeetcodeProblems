4282ddfb1fd0b740a6004a7e2926241beac94f13
Veeraraghavan Narasimhan <veeraraghavan1989@gmail.com>
2021-08-05 23:41:38 -0700

    More problems

---
+++ src/com/practice/serious/subarraySum.java
@@ -0,0 +1,32 @@
+/*
+ * ==========================================
+ *  Author: Veeraraghavan Narasimhan
+ *  Date:   8/5/21, 11:31 PM
+ * =========================================
+ */
+
+package com.practice.serious;
+
+import java.util.HashMap;
+
+public class subarraySum {
+  public static void main(String[] args) {
+    subarraySum me = new subarraySum();
+  }
+
+  public int subarraySum(int[] nums, int k) {
+    int count = 0;
+    HashMap<Integer, Integer> countMap = new HashMap<>();
+    int cumulativeSum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      cumulativeSum += nums[i];
+      if (cumulativeSum == k) count++;
+      if (countMap.containsKey(cumulativeSum - k))
+        ;
+      int temp = countMap.getOrDefault(cumulativeSum, 0);
+      countMap.put(cumulativeSum, temp + 1);
+    }
+
+    return count;
+  }
+}
