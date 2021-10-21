3e64cfbb6f925b5c6a1a3a1e9a226057b8e609f2
Veeraraghavan Narasimhan <veeraraghavan1989@gmail.com>
2021-10-20 23:40:32 -0700

    More LC

--- src/com/practice/serious/ReorderList.java
+++ src/com/practice/serious/ReorderList.java
@@ -7,10 +7,31 @@
 
 package com.practice.serious;
 
+import java.util.Stack;
+
 public class ReorderList {
   public static void main(String[] args) {
     ReorderList me = new ReorderList();
   }
 
-  public void reorderList(ListNode head) {}
+  public void reorderList(ListNode head) {
+    Stack<ListNode> stack = new Stack<>();
+    ListNode first = head;
+    while (first != null) {
+      stack.add(first);
+      first = first.next;
+    }
+    first = head;
+    while (first != null) {
+      ListNode temp = first.next;
+      ListNode end = stack.pop();
+      end.next = temp;
+      first.next = end;
+      first = temp;
+      if (first != null && first.next != end) {
+        first.next = null;
+        break;
+      }
+    }
+  }
 }
